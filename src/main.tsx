c1e1e1f563842d2d28beb2b408502b9bc6ac64d0
shanzhaguo <xiaoshanzhaguo@163.com>
2023-04-03 20:22:39 +0800

    feat: 样式初始化

---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+// 正确的样式引入顺序
+// 样式初始化一般放在最前面
+import "reset-css"
+// UI框架的样式
+// 组件的样式
+import App from './App'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
