5fceebc6b3691754ed21c2f0af7ab56a32fd005c
shanzhaguo <xiaoshanzhaguo@163.com>
2023-04-04 20:51:38 +0800

    feat: scss的模块化管理样式

--- src/components/Comp1/index.tsx
+++ src/components/Comp1/index.tsx
@@ -1,8 +1,10 @@
 // import "./comp1.scss"  // 全局引入
 
+// 模块化引入
+import styles from "./comp1.module.scss" 
 function Comp() {
     return (
-        <div className="box">
+        <div className={styles.box}>
             <p>这是Comp1里面的内容</p>
         </div>
     )
